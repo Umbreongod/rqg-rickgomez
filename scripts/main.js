a60c621da16f115cd38237ac1ad9d618e93af418
umbreongod <yvetterobles31@yahoo.com>
2016-10-17 20:00:16 +0000

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+var header = document.getElementsById("header");
+
+header.style= "background:#777; border: 1px solid #eaea10;";
+
+var title = document.createElement("h1");
+title.textContent="Random Tip Generator"
+header.appendChild(title);
+
+var description = doument.createElement("p");
+description.innerHTML = "This is a description";
+
+header.appendChild()
